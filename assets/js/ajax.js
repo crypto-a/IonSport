f486e2d70ba74f16fe41b2f8bbc66e4a0dc3f9eb
Ali Rahbar <72822222+crypto-a@users.noreply.github.com>
2023-03-18 17:10:04 -0400

    Fixed Bugs and Errors

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -4,8 +4,10 @@ const aboutButton = document.getElementById('aboutButton');
 const contactButton = document.getElementById('contactButton');
 const bookButton = document.getElementById('bookButton');
 
+
 const main = document.querySelector('main');
 const header = document.querySelector('header');
+const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
 
 function resetActive() 
 {
@@ -13,7 +15,38 @@ function resetActive()
     aboutButton.classList.remove("active");
     contactButton.classList.remove("active");
     bookButton.classList.remove("active");
+
 };
+
+function homePageLoaded()
+{
+    const heroBookButton = document.getElementById('heroBookButton');
+
+      heroBookButton.addEventListener('click', (event) => {
+          event.preventDefault()
+          const xhr = new XMLHttpRequest();
+          xhr.onreadystatechange = function() {
+              if (this.readyState === 4 && this.status === 200) 
+              {
+                  main.innerHTML = this.responseText;
+
+                  // remove active class from all buttons
+                  resetActive();
+                  // add active class to homeButton
+                  bookButton.classList.add("active");
+
+                  //fix header style
+                  header.classList.add('header-inner-pages')
+
+                  //fix main style
+                  main.style.marginTop = "50px";
+              }
+          };
+        xhr.open('GET', 'assets/templates/bookingPage.html', true);
+        xhr.send();
+      });
+};
+
 
 //Fill out mainpage when page loads
 
@@ -29,6 +62,10 @@ const xhr = new XMLHttpRequest();
 
             //fix main style
             main.style.marginTop = "0px";
+
+            //Load the event listiner for the main page
+            homePageLoaded();
+
         }
     };
   xhr.open('GET', 'assets/templates/mainPage.html', true);
@@ -55,6 +92,14 @@ homeButton.addEventListener('click', (event) => {
 
             //fix main style
             main.style.marginTop = "0px";
+
+            //Load the event listiner for the main page
+            homePageLoaded();
+
+            // If the mobile nav toggle icon is visible, click it to close the navbar
+            if (mobileNavToggle.offsetParent !== null) {
+                mobileNavToggle.click();
+            }
         }
     };
   xhr.open('GET', 'assets/templates/mainPage.html', true);
@@ -63,6 +108,8 @@ homeButton.addEventListener('click', (event) => {
 
 
 //Ajax for when about button is clicked
+
+
 
 aboutButton.addEventListener('click', (event) => {
     event.preventDefault()
@@ -82,6 +129,11 @@ aboutButton.addEventListener('click', (event) => {
 
             //fix main style
             main.style.marginTop = "50px";
+
+            // If the mobile nav toggle icon is visible, click it to close the navbar
+            if (mobileNavToggle.offsetParent !== null) {
+                mobileNavToggle.click();
+            }
         }
     };
   xhr.open('GET', 'assets/templates/aboutUsPage.html', true);
@@ -109,6 +161,11 @@ contactButton.addEventListener('click', (event) => {
 
             //fix main style
             main.style.marginTop = "50px";
+
+            // If the mobile nav toggle icon is visible, click it to close the navbar
+            if (mobileNavToggle.offsetParent !== null) {
+                mobileNavToggle.click();
+            }
 
         }
     };
@@ -138,8 +195,17 @@ bookButton.addEventListener('click', (event) => {
 
             //fix main style
             main.style.marginTop = "50px";
+
+            // If the mobile nav toggle icon is visible, click it to close the navbar
+            if (mobileNavToggle.offsetParent !== null) {
+                mobileNavToggle.click();
+            }
         }
     };
   xhr.open('GET', 'assets/templates/bookingPage.html', true);
   xhr.send();
 });
+
+//Ajax for when the heroBookVutton is clicked
+
+      
